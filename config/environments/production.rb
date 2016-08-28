f4813d262cec32a9c6b0b09d394e112edbb56678
Ravi kiran K <ravk0001@Synchronoss.com>
2016-08-28 20:13:36 +0530

    add mail setup in prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,18 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.default_url_options = { :host => 'blog-ravikiran.herokuapp.com' }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+   config.action_mailer.delivery_method = :smtp 
+   config.action_mailer.perform_deliveries = true 
+   config.action_mailer.default :charset => "utf-8" 
+   ActionMailer::Base.smtp_settings = { 
+    :address => "smtp.gmail.com", 
+    :port => 587, 
+    :authentication => :plain, 
+    :domain => 'gmail.com', 
+    :user_name => 'ravikabbinale88@gmail.com', 
+    :password => 'test1234@' }
 end
