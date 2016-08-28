10383f1e767f59212aa2266c993a5fcb95826de6
Ravi kiran K <ravk0001@Synchronoss.com>
2016-08-28 20:34:02 +0530

    removed mail setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,17 +38,6 @@ Rails.application.configure do
 
    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-   config.action_mailer.delivery_method = :smtp 
-   config.action_mailer.perform_deliveries = true 
-   config.action_mailer.default :charset => "utf-8" 
-   ActionMailer::Base.smtp_settings = { 
-    :address => "smtp.gmail.com", 
-    :port => 587, 
-    :authentication => :plain, 
-    :domain => 'gmail.com', 
-    :user_name => 'ravikabbinale88@gmail.com', 
-    :password => 'test1234@' }
-
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,14 +78,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-   config.action_mailer.delivery_method = :smtp 
-   config.action_mailer.perform_deliveries = true 
-   config.action_mailer.default :charset => "utf-8" 
-   ActionMailer::Base.smtp_settings = { 
-    :address => "smtp.gmail.com", 
-    :port => 587, 
-    :authentication => :plain, 
-    :domain => 'gmail.com', 
-    :user_name => 'ravikabbinale88@gmail.com', 
-    :password => 'test1234@' }
+  
 end
